9ec82e642640e86bc57d5b75cfdaad69b0ff9d8d
abhishek-ks1719 <114828154+abhishek-ks1719@users.noreply.github.com>
2023-12-21 15:41:23 +0000

    Math_Functions

---
+++ 01_Basics/number_math.js
@@ -0,0 +1,19 @@
+console.clear();
+console.log("some common Math functions");
+console.log(Math.PI);
+console.log(Math.abs(-12.34)) // always return a positive number 
+console.log(Math.ceil(12.0001)) // return the smallest integet greater than the argument value. If it's even 0.0001 it will round it off to make next value
+console.log(Math.floor(0.999)) //return the larget integer less than or equals to the argument value. even if its 0.99 it will be 0.
+console.log(Math.max(12,3,4,15,44)); //retur the largest intger.
+console.log(Math.min(12,13,4,15,44)) //return the smallest integer
+console.log(Math.pow(2,3)) // a^x
+console.log(`Round :${Math.round(2.5)}`) // round off , 
+console.log(Math.random()) // gen
+// let min = 10;
+// let max = 20;
+
+// for(let i=1;i<=100;i++){
+//     let randomNum = (Math.random()*(max-min +1))+min;
+//     console.log(Math.floor(randomNum));
+// }
+
