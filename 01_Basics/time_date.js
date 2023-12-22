17c193550336fe17ab25aa87cf13672de5798a8e
abhishek-ks1719 <114828154+abhishek-ks1719@users.noreply.github.com>
2023-12-22 18:17:33 +0000

    date&time_1

---
+++ 01_Basics/time_date.js
@@ -0,0 +1,11 @@
+console.clear();
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString())
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+    console.log(myTimeStamp.getTime());
